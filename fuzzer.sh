4ce403814ac02efa62ae473740fcbd27060c9282
Ahmed Tareq <63518121+AhmedT1337@users.noreply.github.com>
2022-09-17 11:29:01 +0200

    Update fuzzer.sh

--- fuzzer.sh
+++ fuzzer.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+
+mkdir splitting_the_wordlist
+
+mv $1 splitting_the_wordlist/
+
+cd splitting_the_wordlist
+
+split -n $3 $1
+
+mv $1 ../
+
+url=$2
+
+fuzz()
+{
 for i in $(cat $1)
 do
-url="${2/FUZZ/$i}"
+url_to_go="${url/FUZZ/$i}"
 tempfile=$(mktemp)
-code=$(curl -s $url --write-out '%{http_code}' -o $tempfile)
-echo "$code          $url"
+code=$(curl -s $url_to_go --write-out '%{http_code}' -o $tempfile)
+echo "$code          $url_to_go"
 rm -rf $tempfile
+done
+}
+
+for file in $(ls)
+do
+fuzz $file &
 done
